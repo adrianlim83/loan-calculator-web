9d2335c35098028a84ec8465a8d11555c713d948
snoochew <snoochew@gmail.com>
2021-10-02 12:20:30 +1300

    code format

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -9,30 +9,25 @@ import "./ErrorMessage.css";
  * @returns quote page
  */
 function ErrorMessage(props) {
-
-    return (
-        <label id="errorMessage">
-            {
-                props.message && 
-                <label className='error'>{props.message}</label>
-            }
-
-        </label>
-    );
+  return (
+    <label id="errorMessage">
+      {props.message && <label className="error">{props.message}</label>}
+    </label>
+  );
 }
 
 /**
  * Declare default props' value if it doesn't supply
  */
- ErrorMessage.defaultProps = {    
-    message: '',
-  };
-  
-  /**
-   * Contains the specific props type that can be passing
-   */
-   ErrorMessage.propTypes = {
-    message: PropTypes.string,
-  };
+ErrorMessage.defaultProps = {
+  message: "",
+};
+
+/**
+ * Contains the specific props type that can be passing
+ */
+ErrorMessage.propTypes = {
+  message: PropTypes.string,
+};
 
 export default ErrorMessage;
